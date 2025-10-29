bd9a714500501312a42378650772cdae35c787d1
lumijoe <spacedesign63@gmail.com>
2025-10-29 11:17:31 +0900

    変数let、複数定義

---
+++ main06.js
@@ -0,0 +1,22 @@
+let myName = '鈴木';
+myName = '高橋';
+alert(myName);
+
+let myString = '鈴木2';
+let currentDate = new Date();
+let myFunction = () => console.log('関数です');
+
+let familyName = '鈴木3';
+let firstName = '太郎3';
+let fullName = familyName + firstName;
+console.log(fullName);
+
+let value1 = 100;
+let value2 = value1;
+console.log(value2);
+
+let a = 1,
+    b = 2,
+    c;
+console.log(a + b);
+console.log(c);
