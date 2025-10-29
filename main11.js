920065f37a1d2c4de1f5a585107d67e951b85af4
lumijoe <spacedesign63@gmail.com>
2025-10-29 17:34:22 +0900

    アロー関数:

--- main11.js
+++ main11.js
@@ -21,3 +21,9 @@ console.log(idTest.textContent);
 idTest.textContent = 'テキスト１';
 console.log(idTest);
 console.log(idTest.textContent);
+
+// アロー関数（function~を簡略）
+const myFunction3 = (b) => {
+    return b + 1;
+};
+console.log(myFunction3(2));
