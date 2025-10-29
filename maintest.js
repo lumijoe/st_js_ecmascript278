4ac703a725c747942532c53dc6ebba226626c2f2
lumijoe <spacedesign63@gmail.com>
2025-10-29 13:59:45 +0900

    テストコード、デバッグエラー表示throw

---
+++ maintest.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+    return a + b;
+}
+
+function testAdd() {
+    const result = add(2, 3);
+    if (result !== 5) {
+        throw new Error(`add(a, b)は a と b の和を返すもの, しかし帰ってきたのは ${result}`);
+    }
+    else {
+        console.log('結果OK');
+    }
+}
+
+testAdd();
